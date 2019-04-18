2dff949a864e27720ef9b8134c36d7a5dcb6414b
mhashim6 <m6hashim@gmail.com>
2019-04-18 09:46:41 +0200

    basic line tracking support.

--- chunk.c
+++ chunk.c
@@ -9,18 +9,23 @@ void initChunk(Chunk *chunk)
     chunk->count = 0;
     chunk->capacity = 0;
     chunk->code = NULL;
+    chunk->lines = NULL;
     initValueArray(&chunk->constants);
 }
 
-void writeChunk(Chunk *chunk, uint8_t byte)
+void writeChunk(Chunk *chunk, uint8_t byte, int line)
 {
     if (chunk->capacity < chunk->count + 1)
     {
         int oldCapacity = chunk->capacity;
         chunk->capacity = GROW_CAPACITY(oldCapacity);
         chunk->code = GROW_ARRAY(chunk->code, uint8_t, oldCapacity, chunk->capacity);
-        chunk->code[chunk->count++] = byte;
+        chunk->lines = GROW_ARRAY(chunk->lines, int, oldCapacity, chunk->capacity);
     }
+    chunk->code[chunk->count] = byte;
+    chunk->lines[chunk->count] = line;
+    
+    chunk->count++;
 }
 
 int addConstant(Chunk *chunk, Value value)
@@ -32,6 +37,7 @@ int addConstant(Chunk *chunk, Value value)
 void freeChunk(Chunk *chunk)
 {
     FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+    FREE_ARRAY(int, chunk->lines, chunk->capacity);
     freeValueArray(&chunk->constants);
     initChunk(chunk);
 }
--- main.c
+++ main.c
@@ -7,9 +7,9 @@ int main(int argc, char const *argv[])
     Chunk chunk;
     initChunk(&chunk);
     int const_index = addConstant(&chunk, 1.2);
-    writeChunk(&chunk, OP_CONSTANT);
-    writeChunk(&chunk, const_index);
-    writeChunk(&chunk, OP_RETURN);
+    writeChunk(&chunk, OP_CONSTANT, 1);
+    writeChunk(&chunk, const_index, 1);
+    writeChunk(&chunk, OP_RETURN, 2);
     disassembleChunk(&chunk, "test chunk");
     freeChunk(&chunk);
     return 0;
