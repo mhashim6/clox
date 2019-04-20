ece8984961b44893e654c569320c0e752efa5c87
mhashim6 <m6hashim@gmail.com>
2019-04-20 17:20:45 +0200

    initial implementation for parser.

---
+++ parser.h
@@ -0,0 +1,42 @@
+#include "common.h"
+#include "lexer.h"
+
+typedef struct {
+  Token current;
+  Token previous;
+  bool hadError;
+  bool panicMode;
+} Parser;
+
+typedef enum {
+  PREC_NONE,
+  PREC_ASSIGNMENT,  // =
+  PREC_OR,          // or
+  PREC_AND,         // and
+  PREC_EQUALITY,    // == !=
+  PREC_COMPARISON,  // < > <= >=
+  PREC_TERM,        // + -
+  PREC_FACTOR,      // * /
+  PREC_UNARY,       // ! -
+  PREC_CALL,        // . () []
+  PREC_PRIMARY
+} Precedence;
+
+typedef void (*ParseFn)();
+
+typedef struct {
+  ParseFn prefix;
+  ParseFn infix;
+  Precedence precedence;
+} ParseRule;
+
+void grouping();
+void expression();
+void binary();
+void unary();
+void number();
+
+#define PARSE_RULES_LENGTH 40
+extern ParseRule parsRules[PARSE_RULES_LENGTH];
+
+#define PARSE_RULE(type) (&parsRules[type])
