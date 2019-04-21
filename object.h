9b68670df0c1c9676a44fb505cad4edfcd7e5b23
mhashim6 <m6hashim@gmail.com>
2019-04-21 12:15:21 +0200

    support other lox object types.

---
+++ object.h
@@ -0,0 +1,36 @@
+#ifndef clox_object_h
+#define clox_object_h
+
+#include "common.h"
+#include "value.h"
+
+typedef enum {
+  OBJ_STRING,
+} ObjType;
+
+struct sObj {
+  ObjType type;
+};
+
+struct sObjString {
+  Obj obj;
+  int length;
+  char* chars;
+};
+
+ObjString* takeString(char* chars, int length);
+ObjString* copyString(const char* chars, int length);
+
+void printObject(Value value);
+
+static inline bool isObjType(Value value, ObjType type) {
+  return IS_OBJ(value) && AS_OBJ(value)->type == type;
+}
+
+#define OBJ_TYPE(value) (AS_OBJ(value)->type)
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
+
+#define AS_STRING(value) (((ObjString*)AS_OBJ(value))->chars)
+#define LOX_STRING(value) ((ObjString*)AS_OBJ(value))
+
+#endif
